524d8d1c7bf80fb3ec69674b341679433c2645f4
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-06 13:46:58 +0100

    Create AlbumEditor

---
+++ Framed/AlbumEditor.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumEditor.swift
+//  Framed
+//
+//  Created by Eric on 06/01/2025.
+//
+
+import SwiftUI
+
+struct AlbumEditor: View {
+    @Bindable var album: Album
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumEditor(album: Album(name: "Preview Album", photos: [], speed: 10))
+}
