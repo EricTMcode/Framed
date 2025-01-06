9193d725f5528a26468f11d290e8ecf571e79730
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-06 13:24:10 +0100

    Initial Commit

---
+++ Framed/FramedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramedApp.swift
+//  Framed
+//
+//  Created by Eric on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FramedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
