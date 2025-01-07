6bc11630bc0243dc41b76d918f69e986f92b74db
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-01-07 13:47:30 +0100

    Create AlbumEditor

--- Framed/ContentView.swift
+++ Framed/ContentView.swift
@@ -5,17 +5,23 @@
 //  Created by Eric on 06/01/2025.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Query var albums: [Album]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(albums) { album in
+                    NavigationLink(value: album) {
+                        Text("\(album.name) (\(album.photos.count))")
+                    }
+                }
+            }
+            .navigationTitle("Framed")
         }
-        .padding()
     }
 }
 
